b5e78844adde99d48b02c2353f78cde81d04963a
Omar Camara <117489682+Username273183@users.noreply.github.com>
2023-12-12 15:52:57 -0500

    project

---
+++ project7/script.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Hide the loader once the content is fully loaded
+//     document.getElementById("loader-container").style.opacity = 0;
+//     setTimeout(function () {
+//         document.getElementById("loader-container").style.display = "none";
+//     }, 500);
+// });
+
