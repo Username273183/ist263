bf8c254d168a9fc212af5f7d30c561f6271294c6
Omar Camara <117489682+Username273183@users.noreply.github.com>
2023-12-15 20:00:22 -0500

    completed project

--- project7/script.js
+++ project7/script.js
@@ -1,8 +1,29 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Hide the loader once the content is fully loaded
-//     document.getElementById("loader-container").style.opacity = 0;
-//     setTimeout(function () {
-//         document.getElementById("loader-container").style.display = "none";
-//     }, 500);
-// });
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => {
+    observer.observe(el);
+});
+var icon = document.getElementById('icon');
+
+icon.onclick = function () {
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme")){
+        icon.className = "fa-solid fa-moon fa-2xl";
+        icon.style.color = "#000000";
+        document.getElementById('ghicon').src = "/images/icons8-github-dark.svg";
+    }else{
+        icon.className = "fa-solid fa-sun fa-2xl";
+        icon.style.color = "#ffffff";
+        document.getElementById('ghicon').src = "/images/icons8-github-light.svg";
+    }
+}
 
